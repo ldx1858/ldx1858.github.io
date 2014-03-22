7a50ab670f967b0c6de6c9828780478fa738e836
liudingxin <185860698@qq.com>
2014-03-22 21:47:29 +0800

    this

---
+++ js/tile.js
@@ -0,0 +1,16 @@
+function Tile(position, value) {
+  this.x = position.x;
+  this.y = position.y;
+  this.value = value;
+
+  this.previousPosition = null;
+}
+
+Tile.prototype.savePosition = function () {
+  this.previousPosition = { x: this.x, y: this.y };
+};
+
+Tile.prototype.updatePosition = function (position) {
+  this.x = position.x;
+  this.y = position.y;
+};
